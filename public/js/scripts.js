409b60cbce0092c6ffd702764cd806841f93772d
Alex Sabella <asabs14@gmail.com>
2017-04-04 01:34:40 -0400

    Create Query Language
    - Created query language
    - Added query parser

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,21 +4,22 @@ function httpGetAsync(theUrl, callback) {
         
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "/api/query", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/JSON');
     xhr.onreadystatechange = function () {
         if (this.readyState != 4) return;
         if (this.status == 200) {
             callback(this.responseText);
         }
     };
-    xhr.send(theUrl);
+    xhr.send(JSON.stringify({'query': theUrl}));
 }
 
 function submitQuery() {
 
     const federateQuery = document.getElementById('federate-query').value;
+    console.log(federateQuery);
 
     httpGetAsync(federateQuery, function(response){
-        document.getElementById("query-result").innerHTML = JSON.stringify(JSON.parse(response),null,"\t");
+        document.getElementById("query-result").innerHTML = JSON.stringify(JSON.parse(response),null,4);
     });
 }
--- webserver.js
+++ webserver.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const cache = require("express-cache-response");
 const compression = require('compression');
 const engine = require('express-dot-engine');
@@ -68,7 +69,125 @@ function startWebserver(db) {
         extensions: ["html", "htm"]
     }));
 
-    app.post("/api/query", function (req, res) {
+    let error = "";
+
+    function parseContext (state, line) {
+        state.context = line;
+    }
+
+    function parseFind (state, line) {
+        let query = {};
+        // Split by OR
+        let terms = line.split(/ or /i);
+        let orList = [];
+        // For every OR
+        for (let i = 0; i < terms.length; ++i) {
+            // Split by AND
+            terms[i] = terms[i].trim();
+            terms[i] = terms[i].split(/ and /i);
+            let andList = [];
+            // For every AND
+            for (let j = 0; j < terms[i].length; ++j) {
+                terms[i][j] = terms[i][j].trim();
+                let matches = terms[i][j].match(/^(.+?)([>=<]+)(.+?)$/);
+                if (matches.length !== 4) {
+                    error = `Comparison statement not valid on Find: ${terms[i][j]}`;
+                    return;
+                }
+                matches[1] = matches[1].trim();
+                matches[2] = matches[2].trim();
+                matches[3] = matches[3].trim();
+                let oper = matches[2];
+                switch(oper) {
+                    case ">":
+                        oper = "$gt";
+                        break;
+                    case "<":
+                        oper = "$lt";
+                        break;
+                    case ">=":
+                        oper = "$gte";
+                        break;
+                    case "<=":
+                        oper = "$lte";
+                        break;
+                    case "=":
+                        oper = "$eq";
+                        break;
+                    default:
+                        error = `Equality operator not valid on Find: ${terms[i][j]}`;
+                        return;
+                }
+                if (matches[1].includes("|len")) {
+                    matches[1] = matches[1].slice(0,-4);
+                    andList.push({[matches[1]]: {$size: {[oper]: matches[3]}}});
+                } else {
+                    andList.push({[matches[1]]: {[oper]: matches[3]}});
+                }
+            }
+            if (andList.length === 1) {
+                orList.push(andList[0]);
+            } else {
+                orList.push({$AND: andList});
+            }
+        }
+        if (orList.length === 1) {
+            query = orList[0];
+        } else {
+            query.$OR = orList;
+        }
+        state.result = query;
+
+        return;
+    }
+
+    function parseLookup (state, line) {
+
+    }
+
+    function parseLine (line,error) {
+        let state = {"context": "", "result": {}};
+        lines = line.split("\n");
+        for (var i = 0; i < lines.length; ++i) {
+            let line = lines[i].split(":",2);
+            let fn = line[0];
+            if (line.length <= 1) {
+                error = `No argument given on ${line}`;
+                return "";
+            }
+            let arg = line[1];
+            arg = arg.trim();
+            switch(line[0].trim().toLowerCase()) {
+                case "context":
+                    parseContext(state, arg);
+                    break;
+                case "find":
+                    parseFind(state, arg);
+                    if (error) return "";
+                    break;
+                case "lookup":
+                    parseLookup(state, arg);
+                    break;
+                default:
+                    error = `No function given on ${line}`;
+                    return error;
+            }
+        }
+        return state.result;
+    }
+
+    app.post("/api/query", bodyParser.json(), function (req, res) {
+        let result = parseLine(req.body.query);
+        if (error) {
+            res.send({Error: error});
+        } else {
+            res.send(result);
+        }
+        
+
+        //res.send([[req.body.query],[req.body.query]]);        
+        
+        /*
         let collection = db.collection("designs");
         collection.find({}).toArray(function (err, result) {
             if (err) {
@@ -82,6 +201,7 @@ function startWebserver(db) {
                 res.json("No results found...");
             }
         });
+        */
         //res.json([{"this": "that"},{"this": "that again"}]);
     });
 
